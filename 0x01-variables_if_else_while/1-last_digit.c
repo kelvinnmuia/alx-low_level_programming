7b331928d91b9ba012c8d668813c3fc72e7a3b54
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-16 11:25:48 -0700

    C program that prints last digit of number n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	
 	lastDigit = n % 10;
 
-	if (lastDigit > 5) {
-	printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
-	} else if (lastDigit == 0) {
-	printf("Last Digit of %d is %d and is 0\n", n, lastDigit);
-	} else {
-	printf("Last Digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	if (lastDigit > 5)
+	{
+		printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last Digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	else
+	{		
+		printf("Last Digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 
 	return (0);
