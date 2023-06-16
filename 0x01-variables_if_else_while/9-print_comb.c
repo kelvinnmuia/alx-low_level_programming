2a2092b11ff88ad463c8d1dd83e1d8860f2efbc9
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-16 16:09:07 -0700

    C program that prints combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * Description: starting point for the program execution
+ * Return: returns 0
+ */
+int main(void)
+{
+	int num = 0;
+	/*loops through to print the numbers*/
+	while (num <= 9)
+	{
+		putchar(num + '0');
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
