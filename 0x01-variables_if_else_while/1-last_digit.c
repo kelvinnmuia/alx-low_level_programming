35db4fac6982286f9ec6b4e0e97273cbd874a03e
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-16 10:33:09 -0700

    C program that prints whether number n is positive or negative

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - function
+ * Description: Entry point of the program execution
+ * Return: returns 0
+ */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	
+	lastDigit = n % 10;
+
+	if (lastDigit > 5) {
+	printf("Last Digit of %d is %d and is greater than 5\n", n, lastDigit);
+	} else if (lastDigit == 0) {
+	printf("Last Digit of %d is %d and is 0\n", n, lastDigit);
+	} else {
+	printf("Last Digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+	}
+
+	return (0);
+}
