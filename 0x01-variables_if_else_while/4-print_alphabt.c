45cb90dd0d0fce269da1f45a0e31536fbfeb5d2f
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-16 13:57:41 -0700

    C program that prints alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * Description: starting point for the program execution
+ * Return: returns 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+	/*loops through to print the alphabets*/
+	while (alpha <= 'z')
+	{
+		if(alpha != 'q' && alpha != 'e')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
