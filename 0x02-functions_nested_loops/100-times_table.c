a935c86e44b92ea0ca21bf5b3522aaec6e5575b4
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-21 14:55:56 -0700

    C program to print times table of n

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - function to return time
+ * @n: function argument
+ * Return: returns 0
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return (0);
+	}
+
+	int row = 0;
+
+	while (row <= n)
+	{
+		int colmn = 0;
+
+		while (colmn <= n)
+		{
+			int result = row * colmn;
+
+			int digit = result;
+
+			if (digit >= 10)
+			{
+				while (digit >= 10)
+				{
+					_putchar('0' + digit / 10);
+						digit %= 10;
+				}
+			}
+
+			_putchar('0' + digit);
+
+			if (colmn != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			colmn++;
+		}
+		_putchar('\n');
+		row++;
+	}
+	return (0);
+}
+
