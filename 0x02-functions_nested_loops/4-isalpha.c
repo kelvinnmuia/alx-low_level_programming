e72850f4f3756c1fdfc4c4f86afd1a59e0b71339
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-21 07:48:04 -0700

    C program to check whether a character is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - function to check whether character is lowercase
+ * @c: character to be checked
+ * Return: Returns 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
