f184917b280d265fea6b03dc7b8f4e82a6ac1320
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-21 07:14:45 -0700

    C program to check whether a character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _islower - function to check whether character is lowercase
+ * @c: character to be checked
+ * Return: Returns 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
