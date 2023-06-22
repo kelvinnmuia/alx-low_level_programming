dc2b025e13a17936c868e19cb05ea88ee553ff25
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-22 16:19:47 -0700

    C function to print numbers to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int nums = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	while (nums <= 9)
 	{
-		_putchar(c);
+		_putchar('0' + nums);
+		nums++;
 	}
 
 	_putchar('\n');
