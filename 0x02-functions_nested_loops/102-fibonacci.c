e4f61f95f59a12a087ff21a1dd15a6904c3a0969
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-21 17:18:40 -0700

    C program for printing fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_fibonacci - function
+ * @n: parameter
+ * Return: returns 0
+ */
+void print_fibonacci(int n)
+{
+	int first = 1;
+	int second = 2;
+
+	printf("%d, %d", first, second);
+
+	for (int i = 3; i <= n; i++)
+	{
+		int next = first + second;
+
+		printf(", %d", next);
+
+		first = second;
+		second = next;
+	}
+
+	printf("\n");
+}
+
+/**
+ * main - function
+ * Description: Entry point
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 50;
+
+	print_fibonacci(50);
+	return (0);
+}
