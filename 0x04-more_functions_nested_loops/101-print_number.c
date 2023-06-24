21ba5a18e2eff61ef84f5ca1a11035a91d58cee3
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-23 19:14:46 -0700

    C function that prints integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int number = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
 
-	if ((n / 10) != 0)
+	if ((number / 10) > 0)
 		print_number(n / 10);
 
-	_putchar('0' + n % 10);
+	_putchar('0' + (number % 10));
 
 }
