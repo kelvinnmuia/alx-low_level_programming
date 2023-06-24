b7acf37e1abc016eeb581780fa9ae3043e0150bc
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-23 17:03:09 -0700

    C function to print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - Prints triangle with #
+ * @size: the target size of the triangle
+ * Return: Returns 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 0; j < i; j++)
+			{
+				_putchar('#');
+			}
+
+			if (i == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+}
