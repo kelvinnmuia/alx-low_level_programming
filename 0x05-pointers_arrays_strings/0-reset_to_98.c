21e612f05d3571cda88f35aa1129fe7bc4c8cc16
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-26 08:59:45 -0700

    C function to update a pointers value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - function
+ * @n: Target parameter
+ * Return: Returns 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*Function prototypes*/
+void reset_to_98(int *n);
+int _putchar(char c);
+/*Constants*/
+
+#endif /*MAIN_H*/
