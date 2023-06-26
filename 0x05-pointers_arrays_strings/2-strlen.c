215626fb955be806034a1056382ee11e1d836f1e
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-26 09:40:46 -0700

    C function to return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - function
+ * @s: Target string
+ * Return: Returns 0
+ */
+void _strlen(char *s)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+
+	}
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 /*Function prototypes*/
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 int _putchar(char c);
 /*Constants*/
 
