535360290901ee6ca6ae909cec5169a9dcfcdc04
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-26 10:16:44 -0700

    C function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _puts - function
+ * @str: Target string
+ * Return: Returns 0
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+
+	}
+	_putchar('\n');
+}
