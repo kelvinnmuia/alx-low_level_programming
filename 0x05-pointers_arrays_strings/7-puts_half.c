0a96f2707ce7e1bed1b5261087ff0c3ce33c01b1
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-27 09:41:31 -0700

    C function that splits a string into two halfs

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,12 @@
 void puts2(char *str)
 {
 	int length = _strlen(str);
-	int f_half;
-	int s_half;
 	int i;
 
 	if (length % 2 == 0)
-		f_half = length / 2;
+		i = length / 2;
 	else
-		s_half = (length - 1) / 2;
+		i = (length - 1) / 2;
 	for (i = 0; i < length; i++)
 	{
 		_putchar(str[i]);
