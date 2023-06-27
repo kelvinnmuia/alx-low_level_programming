672e70c023365f972e7d69e87f4e6d920f6604a9
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-27 13:48:11 -0700

    C program for generating passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function
+ * Description: The program's Entry point
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	srand(time(NULL));
+
+	char password[9];
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		int password_type = rand() % 3;
+
+		if (password_type == 0)
+		{
+			password[i] = rand() % 26 + 'a';
+		}
+		else if (password_type == 1)
+		{
+			password[i] = rand() % 26 + 'A';
+		}
+		else
+		{
+			password[i] = rand() % 26 + '0';
+		}
+	}
+	password[8] = '\0';
+	printf("%s\n", password);
+
+	return (0);
+
+}
