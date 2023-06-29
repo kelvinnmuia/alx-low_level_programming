7f31cc6650511c9edf39e7861a18adce40aaace5
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-29 13:25:06 -0700

    C function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - function
+ * @s1: first string
+ * @s2: second string
+ * Return: returns concatnated characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
