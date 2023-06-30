258ad0bc2963e2b01667c48aa8739da1e49821d6
kelvinnmuia <kelvinnmuia@gmail.com>
2023-06-30 14:00:17 -0700

    C function that print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,14 @@
  */
 void print_number(int n)
 {
-	unsigned int nums = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		nums = -nums
+		n = -n
 	}
 
-	if ((nums / 10) != 0)
-		print_number(nums / 10);
+	if ((n / 10) != 0)
+		print_number(n / 10);
 
-	_putchar((nums % 10) + '0');
+	_putchar((n % 10) + '0');
 }
