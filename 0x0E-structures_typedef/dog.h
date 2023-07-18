cd887e008607204943bcc7a5f493159923b48e43
kelvinnmuia <kelvinnmuia@gmail.com>
2023-07-18 13:47:22 -0700

    struct initialization

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - dog typedef
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
