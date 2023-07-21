00a36da8dd116a10b88999ac223ca647c28e813c
kelvinnmuia <kelvinnmuia@gmail.com>
2023-07-21 12:20:50 -0700

    C program to print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - function
+ * @argc: arguments counter
+ * @argv: arguments vecto
+ * Return: Returns 1 or 0
+ */
+int main(int argc, char *argv[])
+{
+	int n = atoi(argv[1]);
+	char *fp;
+
+	if (argc != 2)
+	{
+		puts("Error\n");
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		puts("Error\n");
+		return (2);
+	}
+
+	fp = (char *)main;
+	for (; n > 0; n--, fp++)
+	{
+		printf("%02x", *fp & 0xff);
+		if (n != 1)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
