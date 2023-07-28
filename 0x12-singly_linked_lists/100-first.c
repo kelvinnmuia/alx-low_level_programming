bfd755779cd54a874d655c3bfc45f765290ca7b7
kelvinnmuia <kelvinnmuia@gmail.com>
2023-07-27 19:19:02 -0700

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_lines - function to print string before executing main 
+ * print_lines - function to print string before executing main
  * Description: the functions prints lines of strings
  * Return: returns nothing
  */
@@ -10,5 +10,6 @@ void print_lines(void) __attribute__((constructor));
 
 void print_lines(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
