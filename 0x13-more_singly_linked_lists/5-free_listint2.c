2bd8c38a48ecb1cba3955ce6d8e09917ec3bc8fc
kelvinnmuia <kelvinnmuia@gmail.com>
2023-07-31 13:52:05 -0700

    function that free **head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees the memory
+ * @head: the pointer of the head node
+ * Return: returns nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+}
