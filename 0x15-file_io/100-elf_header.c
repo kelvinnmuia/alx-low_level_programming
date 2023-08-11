1bd79daf568d6cfbf9f46d51d3626db4fcaf2de6
kelvinnmuia <kelvinnmuia@gmail.com>
2023-08-11 09:14:09 -0700

    print elf header information

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,87 +4,86 @@
 #include <unistd.h>
 #include <elf.h>
 #include <fcntl.h>
-#include <string.h>
 
+#define ELF_HEADER_SIZE sizeof(Elf64_Ehdr)
 /**
- * print_error - helper function to print error
- * @errmsg: error message
- * Return: returns nothing
- */
-void print_error(const char *errmsg)
-{
-	fprintf(stderr, "%s\n", errmsg);
-	exit(98);
-}
-/**
- * print_required_info - helper function to print the required info
- * @info: pointer to the required elf info
- * Return: returns the required elf info
- */
-int print_required_info(const char *info)
-{
-	return	(strstr(info, "Magic:") ||
-		 strstr(info, "Class:") ||
-		 strstr(info, "Data:") ||
-		 strstr(info, "Version:") ||
-		 strstr(info, "OS/ABI:") ||
-		 strstr(info, "ABI Version") ||
-		 strstr(info, "Type:") ||
-		 strstr(info, "Entry Point Address:"));
-}
-/**
- * print_elfhd_info - function to display elf header info
- * @filename: pointer to the elf file
- * Return: returns nothing
+ * print_elfhd_info - function to print the elf information
+ * @filename: pointer to the file name
+ * Return: returns elf header information
  */
 void print_elfhd_info(const char *filename)
 {
-	FILE *fp = fopen(filename, "r");
-	char command[256];
-	FILE *readelf_fp = popen(command, "r");
-	char buffer[1024];
-	int counter = 0;
+	int fd = open(filename, O_RDONLY);
+	int is_elf = 1;
+	int i;
+	Elf64_Ehdr hdr;
+	char *class_type, *data_type, *os_abi, *type_str;
 
-	if (!fp)
+	if (fd == -1)
 	{
-		fprintf(stderr, "Error opening the file: %s\n", filename);
+		fprintf(stderr, "Error opening file: %s\n", filename);
 		exit(98);
 	}
-	snprintf(command, sizeof(command), "readelf -h %s", filename);
 
-	if (!readelf_fp)
+	if (read(fd, &hdr, ELF_HEADER_SIZE) != ELF_HEADER_SIZE)
 	{
-		print_error("Error executing readelf command");
+		fprintf(stderr, "Error reading ELF header from file: %s\n", filename);
+		close(fd);
+		exit(98);
 	}
 
-	printf("ELF Header:\n");
-
-	while (fgets(buffer, sizeof(buffer), readelf_fp))
+	for (i = 0; i < EI_MAG3 + 1; i++)
 	{
-		if (print_required_info(buffer))
+		if (hdr.e_ident[i] != ELFMAG[i])
 		{
-			printf(" %s", buffer);
-			counter++;
-		}
-
-		if (counter >= 8)
-		{
+			is_elf = 0;
 			break;
 		}
 	}
 
-	if (counter < 8)
+	if (!is_elf)
 	{
-		print_error("Error reading the elf header");
+		fprintf(stderr, "Error: %s is not an ELF file\n", filename);
+		close(fd);
+		exit(98);
 	}
-	pclose(readelf_fp);
-	fclose(fp);
+
+	printf("Magic:\t");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", hdr.e_ident[i]);
+	}
+	printf("\n");
+
+	class_type = (hdr.e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32";
+	printf("Class:\t %s\n", class_type);
+
+	data_type = (hdr.e_ident[EI_DATA] == ELFDATA2LSB)
+		? "2's complement, little endian"
+		: "Unknown";
+	printf("Data:\t %s\n", data_type);
+
+	printf("Version:\t %d (current)\n", hdr.e_ident[EI_VERSION]);
+
+	os_abi = (hdr.e_ident[EI_OSABI] == ELFOSABI_SYSV)
+		? "UNIX - System V"
+		: "Unknown";
+	printf("OS/ABI:\t %s\n", os_abi);
+
+	printf("ABI Version: %d\n", hdr.e_ident[EI_ABIVERSION]);
+
+	type_str = (hdr.e_type == ET_EXEC) ? "EXEC (Executable file)" : "Unknown";
+	printf("Type: %s\n", type_str);
+
+	printf("Entry point address:\t 0x%lx\n", hdr.e_entry);
+
+	close(fd);
 }
 /**
  * main - program entry point
  * @argc: argument counter
  * @argv: argument vector
- * Return: returns 1 or 0
+ * Return: returns 1 or 0;
  */
 int main(int argc, char *argv[])
 {
@@ -94,6 +93,5 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	print_elfhd_info(argv[1]);
-
 	return (0);
 }
